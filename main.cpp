95f4539c8e5e3c012caa25ace73d00b344f8c0be
Luis Gabriel <luisgabrielcaetano@gmail.com>
2021-07-28 20:00:01 -0300

    finished writing the code for the main.cpp file, which mainly consists or file reading

--- main.cpp
+++ main.cpp
@@ -14,17 +14,21 @@ int main(){
 
     cin >> numAeroportos;
 
+    Grafo linhaAerea(numAeroportos);
+
     cin >> numRotas;
 
+    int i = 0;
 
+    int origem, destino;
 
+    for(i = 0; i < numRotas; i++){
+        cin >> origem;
+        cin >> destino;
+        linhaAerea.construirAresta(origem, destino);
+    }
 
-
-
-
-
-
-
+    cout << linhaAerea.arestasFaltando() << endl;
 
     return 1;
 }
